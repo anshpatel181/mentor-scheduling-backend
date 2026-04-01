cece7b621148ed6ef540661b44013f8710afbafd
ansh46033512 <patelansh6989@gmail.com>
2026-04-01 23:16:21 +0530

    fix cors

--- src/index.js
+++ src/index.js
@@ -18,12 +18,11 @@ const allowedOrigins = [
   "http://localhost:5173",
 ];
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 
